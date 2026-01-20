da2828925ac1090cd1781152492242d15d013734
karulny <yugrankovsky@yandex.ru>
2026-01-20 10:39:03 +0300

    docs

---
+++ config.py
@@ -0,0 +1,31 @@
+# Настройки окна
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SCREEN_TITLE = "RTS Prototype"
+
+# Карта
+MAP_PATH = "resources/maps/map.tmx"
+TILE_SCALING = 1
+
+# Юниты
+UNIT_SPEED = 120
+UNIT_RADIUS = 12
+UNIT_HP = 100
+UNIT_DAMAGE = 10
+UNIT_ATTACK_RANGE = 60
+UNIT_ATTACK_DELAY = 1.0
+UNIT_AGGRO_RANGE = 100
+
+# Команды
+PLAYER_TEAM = 1
+ENEMY_TEAM = 0
+
+# Пути к ресурсам
+CROSSBOWMEN_SPRITESHEET = "resources/units/crossbowmen.png"
+SPRITE_SIZE = 32
+SPRITE_SCALE = 2.0
+
+# Анимация
+ANIMATION_SPEED = 0.1
+WALK_FRAMES = (0, 3)  # start, end
+ATTACK_FRAMES = (4, 6)
